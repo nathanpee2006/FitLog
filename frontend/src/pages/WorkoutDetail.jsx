77d3f618b23deeff40b07acfbf3bc58966e85b4c
nathanpee2006 <152486286+nathanpee2006@users.noreply.github.com>
2025-04-21 21:29:41 +1200

    Display workout data from workout_detail API on React frontend

---
+++ frontend/src/pages/WorkoutDetail.jsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Heading,
+  Tag,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { getWorkoutDetail } from "../endpoints/api";
+
+export default function WorkoutDetail() {
+  const [exercises, setExercises] = useState([]);
+  const { id } = useParams();
+  console.log(id);
+  console.log(exercises);
+
+  // TODO: For each exercise show a table contains sets, previous, weight in kg, reps
+  // FIX: refresh token not refreshing the access token
+  const workoutExercises = exercises.map((exercise) => (
+    <Box key={exercise.id} m="5">
+      <Heading>{exercise.exercise.name}</Heading>
+      <Tag>{exercise.exercise.muscle_group}</Tag>
+      {exercise.exercise.equipment && <Tag>{exercise.exercise.equipment}</Tag>}
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Set</Th>
+              <Th>Previous</Th>
+              <Th isNumeric>kg</Th>
+              <Th isNumeric>Reps</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {exercise.sets.map((set) => (
+              <Tr key={set.set_number}>
+                <Td>{set.set_number}</Td>
+                <Td>
+                  {set.weight}kg x {set.reps}
+                </Td>
+                <Td isNumeric>{set.weight}</Td>
+                <Td isNumeric>{set.reps}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  ));
+
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const exercises = await getWorkoutDetail(id);
+      setExercises(exercises);
+    };
+    fetchWorkout();
+  }, []);
+
+  return workoutExercises;
+}
